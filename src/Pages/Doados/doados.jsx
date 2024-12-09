e9c5751386eb92dd6419a56828f89fac065d772d
Michele-Costaa <michemichele99@gmail.com>
2024-12-09 17:28:34 -0300

    finalizado

--- src/Pages/Doados/doados.jsx
+++ src/Pages/Doados/doados.jsx
@@ -1,10 +1,48 @@
-
-
+import livro from "../../assets/O_protagonista.png";
+import S from "./doados.module.scss";
 
 export default function Doados() {
-    return(
-        <section>
-            <h2>Doados</h2>
-        </section>
-    )
+  return (
+    <section className={S.boxDoados}>
+      <h2>Livros Doados</h2>
+      <section className={S.boxBooks}>
+        <article>
+          <img src={livro} alt="imagem do livro o protagonista" />
+          <h3>O protagonista</h3>
+          <p>Susane Andrade</p>
+          <p>Ficção</p>
+        </article>
+        <article>
+          <img src={livro} alt="imagem do livro o protagonista" />
+          <h3>O protagonista</h3>
+          <p>Susane Andrade</p>
+          <p>Ficção</p>
+        </article>
+        <article>
+          <img src={livro} alt="imagem do livro o protagonista" />
+          <h3>O protagonista</h3>
+          <p>Susane Andrade</p>
+          <p>Ficção</p>
+        </article>
+        <article>
+          <img src={livro} alt="imagem do livro o protagonista" />
+          <h3>O protagonista</h3>
+          <p>Susane Andrade</p>
+          <p>Ficção</p>
+        </article>
+        <article>
+          <img src={livro} alt="imagem do livro o protagonista" />
+          <h3>O protagonista</h3>
+          <p>Susane Andrade</p>
+          <p>Ficção</p>
+        </article>
+        <article>
+          <img src={livro} alt="imagem do livro o protagonista" />
+          <h3>O protagonista</h3>
+          <p>Susane Andrade</p>
+          <p>Ficção</p>
+        </article>
+      </section>
+    </section>
+  );
 }
