1b27488c1f26e263cb534c6263d6e53a249b0400
Michele-Costaa <michemichele99@gmail.com>
2025-04-07 20:47:23 -0300

    finalizado

--- src/Pages/Doados/doados.jsx
+++ src/Pages/Doados/doados.jsx
@@ -1,7 +1,21 @@
 import livro from "../../assets/O_protagonista.png";
 import S from "./doados.module.scss";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function Doados() {
+
+  const [livros, setLivros] = useState([])
+
+  const getLivros = async () => {
+    const response = await axios.get("https://api-livros-rwx6.onrender.com/livros")
+    setLivros(response.data)
+  }
+
+  useEffect(() => {
+    getLivros()
+  }, [])
+
   return (
     <section className={S.boxDoados}>
       <h2>Livros Doados</h2>
@@ -12,36 +26,25 @@ export default function Doados() {
           <p>Susane Andrade</p>
           <p>Ficção</p>
         </article>
-        <article>
-          <img src={livro} alt="imagem do livro o protagonista" />
-          <h3>O protagonista</h3>
-          <p>Susane Andrade</p>
-          <p>Ficção</p>
-        </article>
-        <article>
-          <img src={livro} alt="imagem do livro o protagonista" />
-          <h3>O protagonista</h3>
-          <p>Susane Andrade</p>
-          <p>Ficção</p>
-        </article>
-        <article>
-          <img src={livro} alt="imagem do livro o protagonista" />
-          <h3>O protagonista</h3>
-          <p>Susane Andrade</p>
-          <p>Ficção</p>
-        </article>
-        <article>
-          <img src={livro} alt="imagem do livro o protagonista" />
-          <h3>O protagonista</h3>
-          <p>Susane Andrade</p>
-          <p>Ficção</p>
-        </article>
-        <article>
-          <img src={livro} alt="imagem do livro o protagonista" />
-          <h3>O protagonista</h3>
-          <p>Susane Andrade</p>
-          <p>Ficção</p>
-        </article>
+
+        {livros.map((item)=>(
+          <article key={item.id}>
+            <img src={item.imagem_url} alt="" />
+            <h3>{item.titulo}</h3>
+            <p>{item.categoria}</p>
+            <p>{item.autor}</p>
+          </article>
+        ))}
+
+        {/* {livros.map((item, index) => (
+          <article key={index}>
+            <img src={item.imagem_url} alt="" />
+            <h3>{item.titulo}</h3>
+            <p>{item.categoria}</p>
+            <p>{item.autor}</p>
+          </article>
+        ))} */}
+
       </section>
     </section>
   );
--- src/Pages/QueroDoar/queroDoar.jsx
+++ src/Pages/QueroDoar/queroDoar.jsx
@@ -1,21 +1,65 @@
 import S from './queroDoar.module.scss'
 import livro from '../../assets/Livro.png'
+import axios from 'axios'
+import {useState} from 'react'
 
 export default function QueroDoar() {
+
+    const [titulo, setTitulo] = useState("")
+    const [categoria, setCategoria] = useState("")
+    const [autor, setAutor] = useState("")
+    const [imagem_url, setImagem_url] = useState("")
+
+    const enviarDados = async() => {
+        const urlApi = "https://api-livros-rwx6.onrender.com/doar" 
+
+        const dadosEnviar = {
+            titulo,
+            categoria,
+            autor,
+            imagem_url
+        }
+
+        await axios.post(urlApi, dadosEnviar)
+        alert("Livro cadastrado!!")
+
+        setTitulo("")
+        setCategoria("")
+        setAutor("")
+        setImagem_url("")
+    }
+
+    const capturaTitulo = (e) => {
+        setTitulo(e.target.value)
+        console.log(e.target.value)
+    }
+
+    const capturaCategoria = (e) => {
+        setCategoria(e.target.value)
+    }
+
+    const capturaAutor = (e) => {
+        setAutor(e.target.value)
+    }
+
+    const capturaImagem = (e) => {
+        setImagem_url(e.target.value)
+    }
+
     return(
         <section className={S.principal}>
             <section className={S.container}>
                 <h2>Por favor, preencha o formuláriocom suas informações e as informações do Livro</h2>
-                <form action="">
+                <form onSubmit={(e) => e.preventDefault()}>
                     <div>
                         <img src={livro} alt="imagem de um livro aberto" />
                         <h3>Informações do Livro</h3>
                     </div>
-                    <input type="text" placeholder="Título" />
-                    <input type="text" placeholder="Categoria" />
-                    <input type="text" placeholder="Autor" />
-                    <input type="text" placeholder="Link da Imagem" />
-                    <input className={S.doar} type="submit" value="Doar" />
+                    <input type="text" placeholder="Título" onChange = {capturaTitulo} value={titulo}/>
+                    <input type="text" placeholder="Categoria" onChange = {capturaCategoria} value={categoria}/>
+                    <input type="text" placeholder="Autor" onChange = {capturaAutor} value = {autor}/>
+                    <input type="text" placeholder="Link da Imagem" onChange = {capturaImagem} value = {imagem_url}/>
+                    <input className={S.doar} type="submit" value="Doar" onClick={enviarDados}/>
                 </form>
             </section>
         </section>
